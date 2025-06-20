48d582ba2338a91014a387aae81955835206a3b1
Deepesh <sainideepesh12@gmail.com>
2025-06-20 21:18:28 +0530

    make frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Profile from "./Profile";
+import Layout from "./Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
