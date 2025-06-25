87b4c11f0972fb02cf4a9d9e48e619e459cd3a03
Deepesh <sainideepesh12@gmail.com>
2025-06-25 22:57:20 +0530

    max size of image is 2MB

--- Frontend/src/Profile.jsx
+++ Frontend/src/Profile.jsx
@@ -97,10 +97,28 @@ function Profile() {
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
+
     if (files && files.length > 0) {
-      setFormData({ ...formData, [name]: files[0] });
+      const file = files[0];
+
+      if (file.size > 2 * 1024 * 1024) {
+        alert("File size should be less than 2MB");
+
+        e.target.value = "";
+
+        return;
+      }
+
+      setFormData((prev) => ({
+        ...prev,
+        [name]: file,
+      }));
     } else {
-      setFormData({ ...formData, [name]: value });
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+
       if (name === "userName") {
         setUsername(value);
       }
