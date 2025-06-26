751fca157f9944c42adb49b32c8f4a6904f6dd34
Deepesh <sainideepesh12@gmail.com>
2025-06-26 15:29:20 +0530

    user can post2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,120 +1,3 @@
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Login from "./Login";
-// import Register from "./Register";
-// import Profile from "./Profile";
-// import Layout from "./Layout";
-// import UserProvider from "./context/UserContext";
-// import GithubCallback from "./components/GithubCallback";
-// import GoogleCallback from "./components/GoogleCallback";
-// import LinkedinCallback from "./components/LinkedinCallback";
-// import ProtectedRoute from "./components/ProtectedRoute";
-// import AuthProvider from "./context/AuthContext";
-// import Setting from "./pages/Setting";
-// import Connection from "./pages/Connection";
-// import Post from "./pages/Post";
-// import Message from "./pages/Message";
-// import Notifications from "./pages/Notifications";
-// import MainLayout from "./pages/MainLaout";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         index: true,
-//         element: <Login />,
-//       },
-//       {
-//         path: "/register",
-//         element: <Register />,
-//       },
-//       {
-//         path: "/github-callback",
-//         element: <GithubCallback />,
-//       },
-//       {
-//         path: "google/callback",
-//         element: <GoogleCallback />,
-//       },
-//       {
-//         path: "linkedin/callback",
-//         element: <LinkedinCallback />,
-//       },
-//       {
-//         path: "/profile",
-//         element: (
-//           <ProtectedRoute>
-//             <Profile />
-//           </ProtectedRoute>
-//         ),
-//       },
-//       {
-//         path: "/mainLayout",
-//         element: (
-//           <ProtectedRoute>
-//             <MainLayout />
-//           </ProtectedRoute>
-//         ),
-//       },
-//       {
-//         path: "/app/Message",
-//         element: (
-//           <ProtectedRoute>
-//             <Message />
-//           </ProtectedRoute>
-//         ),
-//       },
-//       {
-//         path: "/app/post",
-//         element: (
-//           <ProtectedRoute>
-//             <Post />
-//           </ProtectedRoute>
-//         ),
-//       },
-//       {
-//         path: "/app/connection",
-//         element: (
-//           <ProtectedRoute>
-//             <Connection />
-//           </ProtectedRoute>
-//         ),
-//       },
-//       {
-//         path: "/notifiction",
-//         element: (
-//           <ProtectedRoute>
-//             <Notifications />
-//           </ProtectedRoute>
-//         ),
-//       },
-
-//       {
-//         path: "/app/setting",
-//         element: (
-//           <ProtectedRoute>
-//             <Setting />
-//           </ProtectedRoute>
-//         ),
-//       },
-//     ],
-//   },
-// ]);
-// function App() {
-//   return (
-//     <>
-//       <AuthProvider>
-//         <UserProvider>
-//           <RouterProvider router={router} />
-//         </UserProvider>
-//       </AuthProvider>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
@@ -133,7 +16,11 @@ import Message from "./pages/Message";
 import Notifications from "./pages/Notifications";
 import MainLayout from "./pages/MainLaout";
 import HomePage from "./HomePage";
-import DisplayPosts from "./displayPosts";
+import DisplayPosts from "./DisplayPosts";
+
+
+
+
 
 const router = createBrowserRouter([
   {
